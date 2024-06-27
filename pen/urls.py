ecbdc1a7b580e57e7083f905c8d705cda4bf6db8
Vivek Babu <vivekbabu0105@gmail.com>
2024-06-28 01:00:16 +0530

    reg all done

--- pen/urls.py
+++ pen/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path
-from pen import views
+from . import views
 
 urlpatterns = [
-    path('', views.index, name='landing_page.html'),
-    # path('signup/', views.SignupPage, name='signup'),
-    # path('login/', views.LoginPage, name='login'),
+    path('', views.index, name='index'),
     path('home/', views.HomePage, name='home'),
     path('logout/', views.LogoutPage, name='logout'),
-    # path('master_reg/', views.MasterReg, name='master_reg'),
-    path('registration', views.registration,name='registration'),
-    path('login', views.login_user,name='login'),
-    path('coReg', views.coReg,name='master_reg'),
-
+    path('registration/', views.registration, name='registration'),
+    path('login/', views.login_user, name='login'),
+    path('coReg/', views.coReg, name='coReg'),
 ]
--- pen/views.py
+++ pen/views.py
@@ -1,4 +1,3 @@
-# views.py
 from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
@@ -18,6 +17,7 @@ def registration(request):
         email = request.POST['email']
         address = request.POST['address']
         password = request.POST['password']
+        password1 = request.POST['password1']
         qualification = request.POST['qualification']
         phone_number = request.POST['phone_number']
         location = request.POST['location']
@@ -25,47 +25,44 @@ def registration(request):
         city = request.POST['city']
         image = request.FILES['image']
 
+        if password != password1:
+            return HttpResponse("Your password and confirm password do not match!")
+
         try:
             usr = User.objects.create_user(
                 username=email, password=password, is_active=1)
             usr.save()
             par = UserReg.objects.create(
-                                        email=email,address=address, user=usr,qualification=qualification,
-                                                     phone_number=phone_number,location=location, state=state, city=city, image=image)
+                email=email, address=address, user=usr, qualification=qualification,
+                phone_number=phone_number, location=location, state=state, city=city, image=image)
             par.save()
-            return redirect('/login')
+            return redirect('login')  # Use named URL
         except:
             msg = 'Something went wrong..'
-            return render(request, 'registration.html')
-
-    else:
-        return render(request, 'registration.html', {"msg": msg})
-
+    return render(request, 'registration.html', {"msg": msg})
 
 def login_user(request):
     msg = ''
+    next_url = request.GET.get('next', 'home')  # Default to home if no next parameter
     if request.method == 'POST':
         email = request.POST['email']
         password = request.POST['password']
         user = authenticate(username=email, password=password)
         if user is not None:
+            login(request, user)
             if user.is_superuser:
                 return redirect("admin:index")
             elif user.is_staff:
                 data = Master.objects.get(email=email)
                 request.session['id'] = data.id
-                return redirect("home")
+                return redirect(next_url)  # Redirect to next URL or home
             else:
                 data = UserReg.objects.get(email=email)
                 request.session['id'] = data.id
-                return redirect("home")
+                return redirect(next_url)  # Redirect to next URL or home
         else:
-            msg = 'Please check the email..'
-            return render(request, 'login.html', {"msg": msg})
-    else:
-        return render(request, 'login.html')
-
-    
+            msg = 'Invalid email or password.'
+    return render(request, 'login.html', {"msg": msg})
 
 def coReg(request):
     msg = ''
@@ -83,26 +80,13 @@ def coReg(request):
                 username=email, password=password, is_active=0, is_staff=1)
             usr.save()
             tut = Master.objects.create(name=name, email=email, phone=phone, address=address,
-                                       qual=qual, field=field, img=img, user=usr)
+                                        qual=qual, field=field, img=img, user=usr)
             tut.save()
-            msg = 'Registration Succesful..'
+            msg = 'Registration Successful..'
         except:
             msg = 'Something went wrong..'
     data = Master.objects.all()
     return render(request, 'coReg.html', {"data": data, "msg": msg})
-
-# def admintutors(request):
-#     msg = ''
-#     data = Master.objects.all()
-#     return render(request, 'admintutors.html', {"data": data, "msg": msg})
-
-# def approvetutor(request):
-#     id = request.GET['id']
-#     status = request.GET['status']
-#     data = User.objects.get(id=id)
-#     data.is_active = status
-#     data.save()
-#     return redirect("/admintutors")
 
 @login_required(login_url='login')
 def LogoutPage(request):
