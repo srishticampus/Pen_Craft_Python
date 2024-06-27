35d26edd94ea0e89fd740d6e93897f429571be93
Vivek Babu <vivekbabu0105@gmail.com>
2024-06-27 16:57:59 +0530

    checkpoint

--- pencraft/urls.py
+++ pencraft/urls.py
@@ -22,7 +22,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
-
+    # path('master/', include('master.urls')),
     path('',include('pen.urls')),
 ]
 
